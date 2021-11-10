f3894d93b330480c4f3685412bcffd27080c5c92
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-11-10 18:47:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
-   return 0;
+
+    int a=0, primo_numero=0, secondo_numero=1, serie=0;
+    cin >> a;
+
+    if ( a>=2 ) {
+        cout << secondo_numero << endl;
+        for (int i=2; i <=a; i++) {
+            serie = primo_numero + secondo_numero;
+            primo_numero = secondo_numero;
+            secondo_numero = serie;
+            cout << serie << endl;
+        }
+
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
